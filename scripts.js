a39745af576b2ce7636d3311ef156bd3543dba74
haniyeamooyan <amooyanhh@gmail.com>
2025-06-05 16:42:21 +0330

    fix: show data

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let commentsDetails = [];
-tableBody = document.getElementById("table-body");
+const tableBody = document.getElementById("table-body");
 
 function loadComments() {
   fetch("https://jsonplaceholder.typicode.com/comments")
@@ -8,20 +8,29 @@ function loadComments() {
     })
     .then((commentText) => {
       commentsDetails = commentText;
+      renderComments();
     })
-    .then(function renderComments() {
-      commentsDetails.forEach((comment) => {
-        const row = `
+    .catch((error) => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+function renderComments() {
+  // Clear existing content
+  tableBody.innerHTML = '';
+  
+  commentsDetails.forEach((comment) => {
+    const row = `
       <tr>
         <td>${comment.id}</td>
         <td>${comment.name}</td>
         <td>${comment.email}</td>
         <td>${comment.body}</td>
       </tr>
-      `;
-        tableBody.innerHTML += row;
-      });
-    });
+    `;
+    tableBody.innerHTML += row;
+  });
 }
 
-renderComments();
+// Load comments when the page loads
+document.addEventListener('DOMContentLoaded', loadComments);
