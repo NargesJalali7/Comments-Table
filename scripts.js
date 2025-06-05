849aeb5b4be457e1544160bbd97c62ded5b7b4e6
Narges Jalali <narges.jalali726@gmail.com>
2025-06-05 16:24:07 +0330

    Rendering the comments

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let commentsDetails = [];
+tableBody = document.getElementById("table-body");
 
 function loadComments() {
   fetch("https://jsonplaceholder.typicode.com/comments")
@@ -7,7 +8,20 @@ function loadComments() {
     })
     .then((commentText) => {
       commentsDetails = commentText;
+    })
+    .then(function renderComments() {
+      commentsDetails.forEach((comment) => {
+        const row = `
+      <tr>
+        <td>${comment.id}</td>
+        <td>${comment.name}</td>
+        <td>${comment.email}</td>
+        <td>${comment.body}</td>
+      </tr>
+      `;
+        tableBody.innerHTML += row;
+      });
     });
 }
 
-loadComments();
+renderComments();
