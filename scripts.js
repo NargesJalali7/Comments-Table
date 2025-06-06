2f64a0ed5e516d16d4b4afaae0ff8aa477bef8f3
Narges Jalali <narges.jalali726@gmail.com>
2025-06-06 11:23:03 +0330

    Creating Pagination

--- scripts.js
+++ scripts.js
@@ -1,28 +1,61 @@
 let commentsDetails = [];
-tableBody = document.getElementById("table-body");
+const tableBody = document.getElementById("table-body");
+const paginationContainer = document.querySelector(".pagination");
+
+const commentsPerPage = 10;
+let currentPage = 1;
 
 function loadComments() {
   fetch("https://jsonplaceholder.typicode.com/comments")
-    .then((response) => {
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((commentText) => {
       commentsDetails = commentText;
-      function renderComments() {
-        commentsDetails.forEach((comment) => {
-          const row = `
+      renderComments();
+      renderPaginationButtons();
+    });
+}
+
+function renderComments() {
+  tableBody.innerHTML = "";
+  const startIndex = (currentPage - 1) * commentsPerPage;
+  const endIndex = startIndex + commentsPerPage;
+  const currentComments = commentsDetails.slice(startIndex, endIndex);
+  let rows = "";
+  currentComments.forEach((comment) => {
+    rows += `
       <tr>
         <td class='numbers'>${comment.id}</td>
         <td>${comment.name}</td>
         <td>${comment.email}</td>
         <td>${comment.body}</td>
       </tr>
-      `;
-          tableBody.innerHTML += row;
-        });
-      }
+    `;
+  });
+
+  tableBody.innerHTML = rows;
+}
+
+function renderPaginationButtons() {
+  paginationContainer.innerHTML = "";
+
+  const pageCount = Math.ceil(commentsDetails.length / commentsPerPage);
+
+  for (let i = 1; i <= pageCount; i++) {
+    const btn = document.createElement("button");
+    btn.innerText = i;
+
+    if (i === currentPage) {
+      btn.classList.add("active");
+    }
+
+    btn.addEventListener("click", () => {
+      currentPage = i;
       renderComments();
+      renderPaginationButtons();
     });
+
+    paginationContainer.appendChild(btn);
+  }
 }
 
 loadComments();
